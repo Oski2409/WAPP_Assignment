bc5b8f06fe93abfda1f5e805cef12e4cf450766f
hafiz <hafiz@LAPTOP-T57HBMRB>
2026-03-03 10:00:02 +0800

    manage users added

--- WAPP_Assignment/ManageUsers.aspx
+++ WAPP_Assignment/ManageUsers.aspx
@@ -1,14 +1,67 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="ManageUsers.aspx.cs" Inherits="WAPP_Assignment.Pages.ManageUsers" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master"
+    AutoEventWireup="true"
+    CodeBehind="ManageUsers.aspx.cs"
+    Inherits="WAPP_Assignment.Pages.ManageUsers" %>
 
 <asp:Content ContentPlaceHolderID="MainContent" runat="server">
 
 <h2 class="text-warning mb-4">Manage Users</h2>
 
 <div class="card bg-dark text-white p-4 shadow">
+
     <asp:GridView ID="gvUsers"
         runat="server"
-        CssClass="table table-dark table-striped">
+        CssClass="table table-dark table-striped"
+        AutoGenerateColumns="False"
+        DataKeyNames="UserID"
+        OnRowEditing="gvUsers_RowEditing"
+        OnRowCancelingEdit="gvUsers_RowCancelingEdit"
+        OnRowUpdating="gvUsers_RowUpdating"
+        OnRowDeleting="gvUsers_RowDeleting">
+
+        <Columns>
+
+            <asp:BoundField DataField="UserID" HeaderText="User ID" ReadOnly="True" />
+
+            <asp:BoundField DataField="FullName" HeaderText="Full Name" />
+
+            <asp:BoundField DataField="Email" HeaderText="Email" />
+
+            <asp:TemplateField HeaderText="Role">
+                <ItemTemplate>
+                    <%# Eval("Role") %>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:DropDownList ID="ddlRole" runat="server">
+                        <asp:ListItem>RegisteredUser</asp:ListItem>
+                        <asp:ListItem>Admin</asp:ListItem>
+                    </asp:DropDownList>
+                </EditItemTemplate>
+            </asp:TemplateField>
+
+            <asp:TemplateField HeaderText="Status">
+                <ItemTemplate>
+                    <%# Eval("AccountStatus") %>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:DropDownList ID="ddlStatus" runat="server">
+                        <asp:ListItem>Active</asp:ListItem>
+                        <asp:ListItem>Suspended</asp:ListItem>
+                    </asp:DropDownList>
+                </EditItemTemplate>
+            </asp:TemplateField>
+
+            <asp:BoundField DataField="RegistrationDate"
+                HeaderText="Registered On"
+                ReadOnly="True"
+                DataFormatString="{0:dd/MM/yyyy}" />
+
+            <asp:CommandField ShowEditButton="True"
+                              ShowDeleteButton="True" />
+
+        </Columns>
     </asp:GridView>
+
 </div>
 
 </asp:Content>
--- WAPP_Assignment/ManageUsers.aspx.cs
+++ WAPP_Assignment/ManageUsers.aspx.cs
@@ -1,17 +1,112 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.UI.WebControls;
 
 namespace WAPP_Assignment.Pages
 {
     public partial class ManageUsers : System.Web.UI.Page
     {
+        string conStr = ConfigurationManager.ConnectionStrings["SmartClicksDB"].ConnectionString;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Admin protection
+            if (Session["Role"] == null || Session["Role"].ToString() != "Admin")
+            {
+                Response.Redirect("~/Login.aspx");
+            }
 
+            if (!IsPostBack)
+            {
+                LoadUsers();
+            }
+        }
+
+        private void LoadUsers()
+        {
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(
+                    "SELECT UserID, FullName, Email, Role, AccountStatus, RegistrationDate FROM Users",
+                    con);
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvUsers.DataSource = dt;
+                gvUsers.DataBind();
+            }
+        }
+
+        protected void gvUsers_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvUsers.EditIndex = e.NewEditIndex;
+            LoadUsers();
+        }
+
+        protected void gvUsers_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvUsers.EditIndex = -1;
+            LoadUsers();
+        }
+
+        protected void gvUsers_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int userId = Convert.ToInt32(gvUsers.DataKeys[e.RowIndex].Value);
+
+            GridViewRow row = gvUsers.Rows[e.RowIndex];
+
+            string fullName = ((TextBox)row.Cells[1].Controls[0]).Text;
+            string email = ((TextBox)row.Cells[2].Controls[0]).Text;
+
+            DropDownList ddlRole = (DropDownList)row.FindControl("ddlRole");
+            DropDownList ddlStatus = (DropDownList)row.FindControl("ddlStatus");
+
+            string role = ddlRole.SelectedValue;
+            string status = ddlStatus.SelectedValue;
+
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                SqlCommand cmd = new SqlCommand(
+                    @"UPDATE Users 
+                      SET FullName=@FullName,
+                          Email=@Email,
+                          Role=@Role,
+                          AccountStatus=@Status
+                      WHERE UserID=@UserID", con);
+
+                cmd.Parameters.AddWithValue("@FullName", fullName);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Role", role);
+                cmd.Parameters.AddWithValue("@Status", status);
+                cmd.Parameters.AddWithValue("@UserID", userId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            gvUsers.EditIndex = -1;
+            LoadUsers();
+        }
+
+        protected void gvUsers_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int userId = Convert.ToInt32(gvUsers.DataKeys[e.RowIndex].Value);
+
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                SqlCommand cmd = new SqlCommand(
+                    "DELETE FROM Users WHERE UserID=@UserID", con);
+
+                cmd.Parameters.AddWithValue("@UserID", userId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadUsers();
         }
     }
 }
